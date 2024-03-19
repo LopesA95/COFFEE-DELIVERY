0795477b67977f2d46dff65f3193b5d597f92953
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-03-19 00:47:41 -0400

    🎉 Projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { Header } from './components/Header'
+import { defaultTheme } from './styles/theme/default'
+import { Outlet } from 'react-router-dom'
+import { CartContextProvider } from './contexts/CartProvider'
+
 export function App() {
   return (
-    <h1>Coffee Delivery</h1>  
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <CartContextProvider>
+        <Header />
+        <Outlet />
+      </CartContextProvider>
+    </ThemeProvider>
   )
 }
-
